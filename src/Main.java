81932bf269d6a3c4536535979d8b390da7452052
naoeovarella <silviocordeirovarella@gmail.com>
2024-08-21 20:46:19 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    Integer idadepessoa = 19;
+    String nomepessoa = "Silvio";
+
+    /*
+    *  >  Maior
+    *  >= Maior igual
+    *  <  Menor
+    *  <= Menor igual
+    *  == é igual
+    *  != é diferente
+    *  equal compara texto
+    * */
+
+    if (idadePessoa > 21){
+        System.out.print("é maior que 21");
+    }else{
+        System.out.println("Não é maior");
+    }
 }
